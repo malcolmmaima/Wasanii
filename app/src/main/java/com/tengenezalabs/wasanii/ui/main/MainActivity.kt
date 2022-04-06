378db4c3a686b92f02501c06ebc272c15b2147f7
malcolmmaima <malcolmmaima@gmail.com>
2022-04-06 15:44:06 +0300

    minor fix

--- app/src/main/java/com/tengenezalabs/wasanii/ui/main/MainActivity.kt
+++ app/src/main/java/com/tengenezalabs/wasanii/ui/main/MainActivity.kt
@@ -2,6 +2,17 @@ package com.tengenezalabs.wasanii.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.view.get
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
+import com.tengenezalabs.wasanii.R
 import com.tengenezalabs.wasanii.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,5 +25,15 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setSupportActionBar(binding.appBarHome.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_home)
+
+        //Initialize the bottom navigation view
+        //create bottom navigation view object
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigatin_view)
+        bottomNavigationView.setupWithNavController(navController
+        )
     }
 }
